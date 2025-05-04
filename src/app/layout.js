d7321920b4caad309297daab015b4b26435f0151
PinsaraPerera <1pawanpinsara@gmail.com>
2025-05-04 22:38:21 +0530

    restrict to the mobile devices

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,15 @@
 // src/app/layout.jsx
-'use client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '@/redux/store';
-import { ToastProvider } from '@/components/ToastContext';
-import './globals.css';
+import Providers from "@/components/Providers";
+import DesktopOnly from "@/components/DesktopOnly";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <ToastProvider>
-              {children}
-            </ToastProvider>
-          </PersistGate>
-        </Provider>
+        <DesktopOnly>
+          <Providers>{children}</Providers>
+        </DesktopOnly>
       </body>
     </html>
   );
---
+++ src/components/Providers.jsx
@@ -0,0 +1,16 @@
+// src/components/Providers.jsx
+"use client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/redux/store";
+import { ToastProvider } from "@/components/ToastContext";
+
+export default function Providers({ children }) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ToastProvider>{children}</ToastProvider>
+      </PersistGate>
+    </Provider>
+  );
+}
